9626ef525cd0ea68d3f6ad807988b3384fb66da0
Bruno Saito <bruno.saito@usp.br>
2017-10-29 13:21:43 -0200

    removed mux_valida_posicao

--- valida_posicao.vhd
+++ valida_posicao.vhd
@@ -1,7 +1,8 @@
--- VHDL do Sistema Digital
+-- VHDL do modulo verificador da posicao da jogada.
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity valida_posicao is
   port(
@@ -15,21 +16,9 @@ end valida_posicao;
 
 architecture estrutural of valida_posicao is
 
-  component mux is
-    port(
-      clock           : in  std_logic;
-      verifica_jogada : in  std_logic;
-      entrada         : in  std_logic_vector(8 downto 0);
-      seletor         : in  std_logic_vector(6 downto 0);
-      saida           : out std_logic
-    );
-  end component;
-
-  signal s_caractere_valido, s_posicao_valida, s_saida : std_logic;
+  signal trinta_um  : std_logic_vector(5 downto 0) := "110001";
 
   begin
-    mux1  : mux port map (clock, verifica_jogada, posicao, caractere, s_saida);
-
-    posicao_valida <= NOT s_saida;
+    posicao_valida <= NOT posicao(to_integer(unsigned(caractere) - unsigned(trinta_um(5 downto 0))));
 
   end estrutural;
