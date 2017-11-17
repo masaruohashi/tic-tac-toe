85f71f338a5216487078a3f8e6dd63c43cbf168d
Rodrigo Masaru Ohashi <ohashi.masaru@gmail.com>
2017-11-17 00:30:21 -0200

    add a MODEM interface component to the data flow of the game interface

--- interface_jogo/fluxo_dados_interface_jogo.vhd
+++ interface_jogo/fluxo_dados_interface_jogo.vhd
@@ -5,17 +5,27 @@ use ieee.std_logic_1164.all;
 
 entity fluxo_dados_interface_jogo is
   port(
-    clock             : in  std_logic;
-    reset             : in  std_logic;
-    jogador_atual     : in  std_logic;                     -- indica o jogador atual do jogo da velha
-    escreve_memoria   : in  std_logic;                     -- habilita a escrita na memoria
-    recebe_dado       : in  std_logic;                     -- habilita a recepção de dados na uart
-    imprime_tabuleiro : in  std_logic;                     -- habilita o circuito de controle de impressao
-    entrada_serial    : in  std_logic;                     -- entrada de dados do terminal para a uart
-    saida_serial      : out std_logic;                     -- saida de dados da uart para o terminal
-    fim_impressao     : out std_logic;                     -- indica o fim da impressao do tabuleiro no terminal
-    fim_recepcao      : out std_logic;                     -- indica o fim da recepção de um caractere do terminal na uart
-    dado_paralelo     : out std_logic_vector(6 downto 0)   -- dados a serem verificados pela logica 
+    clock                   : in  std_logic;
+    reset                   : in  std_logic;
+    jogador_atual           : in  std_logic;                     -- indica o jogador atual do jogo da velha
+    escreve_memoria         : in  std_logic;                     -- habilita a escrita na memoria
+    recebe_dado_jogador     : in  std_logic;                     -- habilita a recepção de dados do jogador na uart
+    imprime_tabuleiro       : in  std_logic;                     -- habilita o circuito de controle de impressao
+    liga_modem              : in  std_logic;                     -- liga a interface do modem para a comunicação com a outra bancada
+    envia_dado              : in  std_logic;                     -- habilita o envio de dados para a outra bancada
+    entrada_serial_jogador  : in  std_logic;                     -- entrada de dados do terminal para a uart
+    CTS                     : in  std_logic;                     -- Clear to send
+    CD                      : in  std_logic;                     -- Carrier Detect
+    RD                      : in  std_logic;                     -- Received Data
+    DTR                     : out std_logic;                     -- Data transfer Ready
+    RTS                     : out std_logic;                     -- Request to send
+    TD                      : out std_logic;                     -- Transmitted data
+    saida_serial_tabuleiro  : out std_logic;                     -- saida de dados da uart para o terminal
+    fim_impressao           : out std_logic;                     -- indica o fim da impressao do tabuleiro no terminal
+    fim_transmissao         : out std_logic;                     -- indica o fim da tranmissao do dado para a outra bancada
+    fim_recepcao_jogador    : out std_logic;                     -- indica o fim da recepção de um caractere do terminal na uart do jogador
+    fim_recepcao_oponente   : out std_logic;                     -- indica o fim da recepção de um caractere do terminal na uart do oponente
+    dado_paralelo           : out std_logic_vector(6 downto 0)   -- dados a serem verificados pela logica
   );
 end fluxo_dados_interface_jogo;
 
@@ -70,19 +80,54 @@ architecture exemplo of fluxo_dados_interface_jogo is
     );
   end component;
 
+  component interface_modem is
+    port(
+      clock              : in  std_logic;
+      reset              : in  std_logic;
+      liga               : in  std_logic;
+      enviar             : in  std_logic;
+      dadoSerial         : in  std_logic;
+      CTS                : in  std_logic;
+      CD                 : in  std_logic;
+      RD                 : in  std_logic;
+      DTR                : out std_logic;
+      RTS                : out std_logic;
+      TD                 : out std_logic;
+      envioOk            : out  std_logic;
+      temDadoRecebido    : out  std_logic;
+      dadoRecebido       : out  std_logic
+    );
+  end component;
+
+  component mapeador_entrada_caractere is
+    port(
+      jogador: in std_logic_vector(6 downto 0);
+      oponente: in std_logic_vector(6 downto 0);
+      jogador_atual: in std_logic;
+      entrada_caractere: out std_logic_vector(6 downto 0)
+    );
+  end component;
+
 signal s_endereco_leitura, s_endereco_escrita: std_logic_vector(6 downto 0);
-signal s_entrada_caractere, s_saida_caractere: std_logic_vector(6 downto 0);
+signal s_entrada_caractere, s_entrada_caractere_jogador, s_entrada_caractere_oponente, s_saida_caractere: std_logic_vector(6 downto 0);
 signal s_jogador_atual: std_logic;
 signal s_uart_livre: std_logic;
 signal s_transmite_dado: std_logic;
 signal s_leitura_memoria: std_logic;
+signal s_entrada_serial_oponente: std_logic;
+signal s_recebe_dado_oponente: std_logic;
+signal s_envia_dado_oponente: std_logic;
+signal s_saida_serial_jogada: std_logic;
 
 begin
 
   controle_impressao: controlador_impressao port map (clock, reset, imprime_tabuleiro, s_uart_livre, s_endereco_leitura, s_leitura_memoria, s_transmite_dado, fim_impressao);
   memoria: memoria_caractere port map (clock, reset, s_leitura_memoria, escreve_memoria, jogador_atual, s_endereco_leitura, s_endereco_escrita, s_saida_caractere);
   mapeador_char: mapeador_caractere port map (s_entrada_caractere, s_endereco_escrita);
-  uart_jogador: uart port map (clock, reset, entrada_serial, recebe_dado, s_transmite_dado, s_saida_caractere, saida_serial, s_entrada_caractere, fim_recepcao, open, s_uart_livre);
+  uart_jogador: uart port map (clock, reset, entrada_serial_jogador, recebe_dado_jogador, s_transmite_dado, s_saida_caractere, saida_serial_tabuleiro, s_entrada_caractere_jogador, fim_recepcao_jogador, open, s_uart_livre);
+  mapeia_entrada_caractere: mapeador_entrada_caractere port map (s_entrada_caractere_jogador, s_entrada_caractere_oponente, jogador_atual, s_entrada_caractere);
+  uart_oponente: uart port map (clock, reset, s_entrada_serial_oponente, s_recebe_dado_oponente, s_envia_dado_oponente, s_entrada_caractere_jogador, s_saida_serial_jogada, s_entrada_caractere_oponente, fim_recepcao_oponente, open, fim_transmissao);
+  modem_interface: interface_modem port map (clock, reset, liga_modem, envia_dado, s_saida_serial_jogada, CTS, CD, RD, DTR, RTS, TD, s_envia_dado_oponente, s_recebe_dado_oponente, s_entrada_serial_oponente);
 
   dado_paralelo <= s_entrada_caractere;
 
--- interface_jogo/interface_jogo.vhd
+++ interface_jogo/interface_jogo.vhd
@@ -8,10 +8,17 @@ entity interface_jogo is
     clock                 : in std_logic;
     reset                 : in std_logic;
     start                 : in std_logic;
+    jogador               : in std_logic;
     fim_recepcao          : in std_logic;
     recebe_dado           : in std_logic;
     guarda_dado           : in std_logic;
     entrada_serial        : in std_logic;
+    CTS                   : in std_logic;
+    CD                    : in std_logic;
+    RD                    : in std_logic;
+    DTR                   : out std_logic;
+    RTS                   : out std_logic;
+    TD                    : out std_logic;
     jogador_atual         : out std_logic;
     saida_serial          : out std_logic;
     habilita_logica       : out std_logic;
@@ -28,39 +35,57 @@ architecture estrutural of interface_jogo is
       clock               : in std_logic;
       reset               : in std_logic;
       start               : in std_logic;
-      fim_impressao       : in std_logic;
-      fim_recepcao        : in std_logic;
-      imprime_tabuleiro   : out std_logic;
-      recebe_dado         : out std_logic;
-      jogador_atual       : out std_logic;
+      jogador             : in std_logic;   -- indica se o jogador é o primeiro a jogar ou o segundo
+      fim_impressao       : in std_logic;   -- indica que o tabuleiro terminou de ser impresso
+      fim_recepcao        : in std_logic;   -- indica que um caractere terminou de ser recebido
+      fim_transmissao     : in std_logic;   -- indica que um caractere terminou de ser eniado para a outra bancada
+      liga_modem          : out std_logic;   -- indica que a interface do modem deve ser ligada
+      imprime_tabuleiro   : out std_logic;  -- habilita a impressao do tabuleiro
+      envia_jogada        : out std_logic;  -- habilita o envio da jogada para a outra bancada
+      recebe_dado         : out std_logic;  -- habilita a recepção de um caractere do terminal
+      jogador_atual       : out std_logic;  -- indica o jogador atual do jogo da velha
       dep_estados         : out std_logic_vector(2 downto 0)
     );
   end component;
 
   component fluxo_dados_interface_jogo is
     port(
-      clock             : in  std_logic;
-      reset             : in  std_logic;
-      jogador_atual     : in  std_logic;
-      escreve_memoria   : in  std_logic;
-      recebe_dado       : in  std_logic;
-      imprime_tabuleiro : in  std_logic;
-      entrada_serial    : in  std_logic;
-      saida_serial      : out std_logic;
-      fim_impressao     : out std_logic;
-      fim_recepcao      : out std_logic;
-      dado_paralelo     : out std_logic_vector(6 downto 0) 
+      clock                   : in  std_logic;
+      reset                   : in  std_logic;
+      jogador_atual           : in  std_logic;                     -- indica o jogador atual do jogo da velha
+      escreve_memoria         : in  std_logic;                     -- habilita a escrita na memoria
+      recebe_dado_jogador     : in  std_logic;                     -- habilita a recepção de dados do jogador na uart
+      imprime_tabuleiro       : in  std_logic;                     -- habilita o circuito de controle de impressao
+      liga_modem              : in  std_logic;                     -- liga a interface do modem para a comunicação com a outra bancada
+      envia_dado              : in  std_logic;                     -- habilita o envio de dados para a outra bancada
+      entrada_serial_jogador  : in  std_logic;                     -- entrada de dados do terminal para a uart
+      CTS                     : in  std_logic;                     -- Clear to send
+      CD                      : in  std_logic;                     -- Carrier Detect
+      RD                      : in  std_logic;                     -- Received Data
+      DTR                     : out std_logic;                     -- Data transfer Ready
+      RTS                     : out std_logic;                     -- Request to send
+      TD                      : out std_logic;                     -- Transmitted data
+      saida_serial_tabuleiro  : out std_logic;                     -- saida de dados da uart para o terminal
+      fim_impressao           : out std_logic;                     -- indica o fim da impressao do tabuleiro no terminal
+      fim_transmissao         : out std_logic;                     -- indica o fim da tranmissao do dado para a outra bancada
+      fim_recepcao_jogador    : out std_logic;                     -- indica o fim da recepção de um caractere do terminal na uart do jogador
+      fim_recepcao_oponente   : out std_logic;                     -- indica o fim da recepção de um caractere do terminal na uart do oponente
+      dado_paralelo           : out std_logic_vector(6 downto 0)   -- dados a serem verificados pela logica
     );
   end component;
 
   signal s_fim_impressao, s_imprime_tabuleiro: std_logic;
   signal s_jogador_atual: std_logic;
+  signal s_fim_recepcao_jogador, s_fim_recepcao_oponente: std_logic;
+  signal s_liga_modem, s_envia_jogada: std_logic;
+  signal s_fim_transmissao: std_logic; 
 
 begin
 
-  UC: unidade_controle_interface_jogo port map (clock, reset, start, s_fim_impressao, fim_recepcao, s_imprime_tabuleiro, habilita_logica, s_jogador_atual, estados);
-  FD: fluxo_dados_interface_jogo port map(clock, reset, s_jogador_atual, guarda_dado, recebe_dado, s_imprime_tabuleiro, entrada_serial, saida_serial, s_fim_impressao, habilita_verificacao, dado_paralelo);
+  UC: unidade_controle_interface_jogo port map (clock, reset, start, jogador, s_fim_impressao, fim_recepcao, s_fim_transmissao, s_liga_modem, s_imprime_tabuleiro, s_envia_jogada, habilita_logica, s_jogador_atual, estados);
+  FD: fluxo_dados_interface_jogo port map(clock, reset, s_jogador_atual, guarda_dado, recebe_dado, s_imprime_tabuleiro, s_liga_modem, s_envia_jogada, entrada_serial, CTS, CD, RD, DTR, RTS, TD, saida_serial, s_fim_impressao, s_fim_transmissao, s_fim_recepcao_jogador, s_fim_recepcao_oponente, dado_paralelo);
 
   jogador_atual <= s_jogador_atual;
+  habilita_verificacao <= s_fim_recepcao_jogador or s_fim_recepcao_oponente;
 
 end estrutural;
